44a12f54fae4e9d8b8ebddda6ca575c2ae14c95e
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-13 07:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  string.toLowerCase === `string`;
+  string.toLowerCase() === `string`;
   return "I can't hear you!";
 }
 
 function sayHiToGrandma(string){
-  string.toUpperCase === `string`;
+  string.toUpperCase() === `string`;
   return "YES INDEED!"
 }
 
